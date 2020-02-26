fe8cffb6f3e65ed0098c8b7f14120e690e7efd5d
KiChang Shin <kc7851@gmail.com>
2020-02-27 01:47:26 +0900

    Excel 다운로드 기능 추가
    null일 경우 공백되도록 함.
    값이 숫자일 경우 cell type을 numeric으로 함

---
+++ src/main/java/com/skc/excel/controller/ExcelController.java
@@ -0,0 +1,99 @@
+package com.skc.excel.controller;
+
+import com.skc.excel.service.ExcelService;
+import com.skc.excel.utils.ExcelMakeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+public class ExcelController {
+
+    @Autowired
+    private ExcelService excelService;
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/upload")
+    public ModelAndView excelUpload() {
+        ModelAndView modelAndView = new ModelAndView("upload");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/download")
+    public ModelAndView excelDownload() {
+        ModelAndView modelAndView = new ModelAndView("download");
+
+        return modelAndView;
+    }
+
+    @PostMapping("/excelUpload")
+    public String excelUploadPost(@RequestParam MultipartFile excelFile)  throws Exception{
+        String extension = Objects.requireNonNull(
+                excelFile.getOriginalFilename()).substring(
+                        excelFile.getOriginalFilename().lastIndexOf(".") + 1);
+
+        if(excelFile==null || excelFile.isEmpty() || !extension.contains("xls")){
+            throw new RuntimeException("엑셀파일을 선택 해 주세요.");
+        }
+
+        String userHomePath = System.getProperty("java.io.tmpdir");
+
+        File destFile = new File(userHomePath+excelFile.getOriginalFilename());
+
+        try{
+            excelFile.transferTo(destFile);
+        }catch(IllegalStateException | IOException e){
+            throw new RuntimeException(e.getMessage(),e);
+        }
+
+        excelService.excelUpload(destFile);
+
+        return "redirect:/upload";
+    }
+
+    @PostMapping(value = "/excelDownload")
+    public void excelDownloadPost(
+            HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
+
+        String[] strArrHeader = {"순번","회원 ID","회원명"};
+        String filename = "엑셀파일이름"+ System.currentTimeMillis();
+
+        //이 부분에 실제 엑셀에 넣을 데이터를 구성하면 됨.
+        List<HashMap<String, String>> hashMapList = new ArrayList<>();
+        HashMap<String, String> user1 = new HashMap<>();
+        user1.put("순번", "1");
+        user1.put("회원 ID", "id1");
+        user1.put("회원명", "회원1");
+        HashMap<String, String> user2 = new HashMap<>();
+        user2.put("순번", "2");
+        user2.put("회원 ID", null);
+        user2.put("회원명", "회원2");
+        hashMapList.add(user1);
+        hashMapList.add(user2);
+
+        ExcelMakeUtils.buildExcelDocument(hashMapList,filename,strArrHeader,request,response);
+
+    }
+
+}
---
+++ src/main/java/com/skc/excel/utils/ExcelMakeUtils.java
@@ -0,0 +1,89 @@
+package com.skc.excel.utils;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExcelMakeUtils {
+    private static final String CONTENT_TYPE_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+    public static Workbook buildExcelDocument(List<HashMap<String, String>> model, String fileName, String[] header, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        try {
+            String browser = request.getHeader("User-Agent");
+            //파일 인코딩
+            if (browser.contains("MSIE") || browser.contains("Trident")
+                    || browser.contains("Chrome")) {
+                fileName = URLEncoder.encode(fileName, "UTF-8").replaceAll("\\+",
+                        "%20");
+            } else {
+                fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+            }
+        } catch (UnsupportedEncodingException ex) {
+            System.out.println("UnsupportedEncodingException");
+        }
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Pragma", "public");
+        response.setHeader("Expires", "0");
+        response.setHeader("Content-Disposition", "attachment; filename = " + fileName + System.currentTimeMillis() + ".xlsx");
+
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet(fileName);
+
+        Row row = null;
+        int rowCount = 0;
+        int cellCount = 0;
+
+        row = sheet.createRow(rowCount++);
+
+        for (int i = 0; i < header.length; i++)
+            row.createCell(cellCount++).setCellValue(header[i]);
+
+        for (HashMap<String, String> vo : model) {
+            row = sheet.createRow(rowCount++);
+            cellCount = 0;
+            for (int idx = 0; idx < vo.size(); idx++) {
+                String value = vo.get(header[idx]);
+                if (value == null) value = "";
+
+                if(isNumeric(value)) {
+                    row.createCell(cellCount++, CellType.NUMERIC).setCellValue(Double.parseDouble(value));
+                }else {
+                    row.createCell(cellCount++).setCellValue(value);
+                }
+            }
+        }
+
+        ServletOutputStream out = response.getOutputStream();
+        out.flush();
+        workbook.write(out);
+        out.flush();
+
+        if (workbook instanceof SXSSFWorkbook) {
+            ((SXSSFWorkbook) workbook).dispose();
+        }
+
+        return workbook;
+    }
+
+    private static boolean isNumeric(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
